356e1e90c4873b65be24691e27f025c8cb4b4fb7
Tamnya <tamnyasharma12@gmail.com>
2026-03-05 11:29:58 +0530

    Updated HelloWorld program

---
+++ basics/HelloWorld.java
@@ -0,0 +1,10 @@
+//Boilerplate code for a Java program that prints "Hello World!" to the console.
+public class HelloWorld { // class defined with name HelloWorld. Always the name of the class should be
+                          // same as the name of the file.
+    public static void main(String[] args) { // main method is the entry point of the program. It is where the program
+                                             // starts execution.
+        System.out.println("Hello World!"); // System is a class in java.lang package. out is a static member of System
+                                            // class and it is an instance of PrintStream class. println is a method of
+                                            // PrintStream class that prints
+    }
+}
